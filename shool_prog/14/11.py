41e9316004c6437e89d86190993f5a311b3b53ec
Serega Gorbunov <gorbunov.sergei0808@gmail.com>
2025-11-30 21:38:13 +0300

    Update 11.py

--- shool_prog/14/11.py
+++ shool_prog/14/11.py
@@ -1,8 +1,7 @@
-first_digit = '55113x'
-second_digit = '7xx580'
 k = 0
-for x in range(0, 100000):
-    if abs(int(first_digit.replace('x', str(x))) - int(second_digit.replace('xx', str(x)))) < 1000000:
-        k += 1
+for x in range(6, 80):
+    n1 = 5 * x**4 + 5 * x**3 + 1 * x**2 + 1 * x + 3
+    n2 = 7 * 80**3 + x * 80**2 + x * 80 + 5
+    k += int(abs(n1 - n2) <= 1_000_000)
 print(k)
-# 10
+# 4
