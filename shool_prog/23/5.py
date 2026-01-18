ea113277055d61add9391ad04ca99231ce934b14
Serega Gorbunov <gorbunov.sergei0808@gmail.com>
2026-01-18 20:20:09 +0300

    .

--- shool_prog/23/5.py
+++ shool_prog/23/5.py
@@ -1,10 +1,9 @@
-def f(st, fi):
-    if st > fi:
-        return 0
-    if st == fi or st == 2:
+def f(x, y, st):
+    if x == y and st[-2] == '2':
         return 1
-    if st < fi:
-        return f(st + 1, fi) + f(st + 2, fi)
-
-print(f(3, 18))
-# 987
+    elif x > y or x == y and st[-2] == '1':
+        return 0
+    else:
+        return f(x + 1, y, st + '1') + f(x + 2, y, st + '2')
+print(f(3,18,''))
+# 377
